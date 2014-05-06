f02162807c08e245c06f91d8c5ebd6748a3b301c
Daniel Imfeld <daniel@danielimfeld.com>
2014-05-05 20:53:16 -0500

    Actually fill in code.

--- post.go
+++ post.go
@@ -1,21 +1,77 @@
 package main
 
 import (
+	"bufio"
+	"bytes"
+	"fmt"
 	"os"
+	"strings"
+	"time"
 )
 
 type Post struct {
-	sourcePath string
-	timestamp  time.Time
-	title      string
-	tags       []string
-	content    string
+	SourcePath string
+	Title      string
+	Timestamp  time.Time
+	Tags       []string
+	Content    []byte
 }
 
-func NewPost(filePath string, readContent bool) (*Post, error) {
+// NewPost reads a post from disk and returns a Post containing its data.
+// If readContent is false, only the header of the post is read.
+func NewPost(filePath string, readContent bool) (p *Post, err error) {
 	f, err := os.Open(filePath)
 	if err != nil {
 		return nil, err
 	}
+	defer f.Close()
 
+	p = new(Post)
+	p.SourcePath = filePath
+
+	reader := bufio.NewReader(f)
+
+	line, err := reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	p.Title = string(line[0 : len(line)-1])
+
+	line, err = reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+
+	p.Timestamp, err = time.Parse(time.RFC822, line[0:len(line)-1])
+	if err != nil {
+		return
+	}
+
+	line, err = reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	p.Tags = strings.Split(line, ",")
+	for i := range p.Tags {
+		p.Tags[i] = strings.Title(strings.TrimSpace(p.Tags[i]))
+	}
+
+	line, err = reader.ReadString('\n')
+	if err != nil {
+		return
+	}
+	if len(line) != 1 {
+		return p, fmt.Errorf("Unexpected input %s", string(line))
+	}
+
+	if readContent {
+		buf := &bytes.Buffer{}
+		_, err = buf.ReadFrom(reader)
+		if err != nil {
+			return
+		}
+		p.Content = buf.Bytes()
+	}
+
+	return
 }
